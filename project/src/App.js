4e280b0333cb15d0964f56aacdc5562dbb91db4a
HTGYryanz7 <114461721+HTGYryanz7@users.noreply.github.com>
2024-02-21 22:35:02 -0600

    Login checks simple array if id and pwd exist

--- project/src/App.js
+++ project/src/App.js
@@ -1,30 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {useState} from "react"
+
+var Usernames = new Array("John", "Bob");
+var Passwords = new Array("password", "hi");
+
+
+
 function App() {
+  const [name, setName] = useState("");
+  const [pwd, setPWD] = useState("");
+  const [showName, setShowName] = useState(false);
+  const [showPWD, setShowPWD] = useState(false);
+  function handleSubmit(e){
+    e.preventDefault();
+    for(let i=0; i<Usernames.length; i++){
+      if(name == Usernames[i]){
+          setShowName(true);
+      }
+    }
+    for(let i=0; i<Passwords.length; i++){
+      if(pwd == Passwords[i]){
+          setShowPWD(true);
+      }
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
         
         <form>
           <label for="UserID">UserID:</label>
-          <input type="text" id="userid" name="userid"></input>
+          <input type="text" id="userid" name="userid" value={name} 
+            onChange={(e)=>setName(e.target.value)}
+          ></input>
           <br></br>
           <label for="Password">Password:</label>
-          <input type="text" id="password" name="password"></input>
+          <input type="text" id="password" name="password" value={pwd} 
+            onChange={(e)=>setPWD(e.target.value)}></input>
           <br></br>
-          <input type="submit"></input>
+          <button onClick={handleSubmit} type="submit">
+            Submit
+          </button>
+          {showName === true && <p>{name}</p>}
+          {showPWD === true && <p>{pwd}</p>}
         </form>
         
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      
       
     </div>
   );
