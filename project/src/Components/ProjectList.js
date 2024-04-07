727532570cc843ced3c1e0d0e01c097233de7bb5
Hirsch <hirsch.prasad@gmail.com>
2024-04-07 17:56:54 -0500

    Fixing join project

--- project/src/Components/ProjectList.js
+++ project/src/Components/ProjectList.js
@@ -40,7 +40,7 @@ const ProjectList = () => {
     const response = await fetch(url, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ joinProjectId })
+        body: JSON.stringify({ joinProjectId, "UserId": "Hirsch" })
     });
     const data = await response.json();
 
